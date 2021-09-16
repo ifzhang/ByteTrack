27c15470219896dd7889e8ed912208ed7f55125c
Yifu Zhang <953260160@qq.com>
2021-09-16 15:08:36 +0800

    add tracking demo

--- tools/demo_track.py
+++ tools/demo_track.py
@@ -12,6 +12,9 @@ from yolox.data.data_augment import preproc
 from yolox.data.datasets import COCO_CLASSES
 from yolox.exp import get_exp
 from yolox.utils import fuse_model, get_model_info, postprocess, vis
+from yolox.utils.visualize import plot_tracking
+from yolox.tracker.det_tracker import DETTracker
+from yolox.tracking_utils.timer import Timer
 
 import argparse
 import os
@@ -29,7 +32,8 @@ def make_parser():
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
 
     parser.add_argument(
-        "--path", default="./assets/dog.jpg", help="path to images or video"
+        #"--path", default="./datasets/mot/train/MOT17-05-FRCNN/img1", help="path to images or video"
+        "--path", default="./videos/palace.mp4", help="path to images or video"
     )
     parser.add_argument("--camid", type=int, default=0, help="webcam demo camera id")
     parser.add_argument(
@@ -49,7 +53,7 @@ def make_parser():
     parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt for eval")
     parser.add_argument(
         "--device",
-        default="cpu",
+        default="gpu",
         type=str,
         help="device to run our model, can either be cpu or gpu",
     )
@@ -77,6 +81,11 @@ def make_parser():
         action="store_true",
         help="Using TensorRT model for testing.",
     )
+    # tracking args
+    parser.add_argument("--track_thresh", type=float, default=0.4, help="tracking confidence threshold")
+    parser.add_argument("--track_buffer", type=int, default=30, help="the frames for keep lost tracks")
+    parser.add_argument("--match_thresh", type=int, default=0.9, help="matching threshold for tracking")
+    parser.add_argument('--min-box-area', type=float, default=100, help='filter out tiny boxes')
     return parser
 
 
@@ -91,6 +100,19 @@ def get_image_list(path):
     return image_names
 
 
+def write_results(filename, results):
+    save_format = '{frame},{id},{x1},{y1},{w},{h},{s},-1,-1,-1\n'
+    with open(filename, 'w') as f:
+        for frame_id, tlwhs, track_ids, scores in results:
+            for tlwh, track_id, score in zip(tlwhs, track_ids, scores):
+                if track_id < 0:
+                    continue
+                x1, y1, w, h = tlwh
+                line = save_format.format(frame=frame_id, id=track_id, x1=round(x1, 1), y1=round(y1, 1), w=round(w, 1), h=round(h, 1), s=round(score, 2))
+                f.write(line)
+    logger.info('save results to {}'.format(filename))
+
+
 class Predictor(object):
     def __init__(
         self,
@@ -100,6 +122,7 @@ class Predictor(object):
         trt_file=None,
         decoder=None,
         device="cpu",
+        fp16=False
     ):
         self.model = model
         self.cls_names = cls_names
@@ -109,6 +132,7 @@ class Predictor(object):
         self.nmsthre = exp.nmsthre
         self.test_size = exp.test_size
         self.device = device
+        self.fp16 = fp16
         if trt_file is not None:
             from torch2trt import TRTModule
 
@@ -121,7 +145,7 @@ class Predictor(object):
         self.rgb_means = (0.485, 0.456, 0.406)
         self.std = (0.229, 0.224, 0.225)
 
-    def inference(self, img):
+    def inference(self, img, timer):
         img_info = {"id": 0}
         if isinstance(img, str):
             img_info["file_name"] = os.path.basename(img)
@@ -137,18 +161,21 @@ class Predictor(object):
         img, ratio = preproc(img, self.test_size, self.rgb_means, self.std)
         img_info["ratio"] = ratio
         img = torch.from_numpy(img).unsqueeze(0)
+        img = img.float()
         if self.device == "gpu":
             img = img.cuda()
+            if self.fp16:
+                img = img.half()  # to FP16
 
         with torch.no_grad():
-            t0 = time.time()
+            timer.tic()
             outputs = self.model(img)
             if self.decoder is not None:
                 outputs = self.decoder(outputs, dtype=outputs.type())
             outputs = postprocess(
                 outputs, self.num_classes, self.confthre, self.nmsthre
             )
-            logger.info("Infer time: {:.4f}s".format(time.time() - t0))
+            #logger.info("Infer time: {:.4f}s".format(time.time() - t0))
         return outputs, img_info
 
     def visual(self, output, img_info, cls_conf=0.35):
@@ -176,20 +203,45 @@ def image_demo(predictor, vis_folder, path, current_time, save_result):
     else:
         files = [path]
     files.sort()
+    tracker = DETTracker(args, frame_rate=30)
+    timer = Timer()
+    frame_id = 0
+    results = []
     for image_name in files:
-        outputs, img_info = predictor.inference(image_name)
-        result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
+        if frame_id % 20 == 0:
+            logger.info('Processing frame {} ({:.2f} fps)'.format(frame_id, 1. / max(1e-5, timer.average_time)))
+        outputs, img_info = predictor.inference(image_name, timer)
+        online_targets = tracker.update(outputs[0], [img_info['height'], img_info['width']], exp.test_size)
+        online_tlwhs = []
+        online_ids = []
+        online_scores = []
+        for t in online_targets:
+            tlwh = t.tlwh
+            tid = t.track_id
+            vertical = tlwh[2] / tlwh[3] > 1.6
+            if tlwh[2] * tlwh[3] > args.min_box_area and not vertical:
+                online_tlwhs.append(tlwh)
+                online_ids.append(tid)
+                online_scores.append(t.score)
+        timer.toc()
+        # save results
+        results.append((frame_id + 1, online_tlwhs, online_ids, online_scores))
+        online_im = plot_tracking(img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id + 1,
+                                          fps=1. / timer.average_time)
+
+        #result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
         if save_result:
             save_folder = os.path.join(
                 vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
             )
             os.makedirs(save_folder, exist_ok=True)
             save_file_name = os.path.join(save_folder, os.path.basename(image_name))
-            logger.info("Saving detection result in {}".format(save_file_name))
-            cv2.imwrite(save_file_name, result_image)
+            cv2.imwrite(save_file_name, online_im)
         ch = cv2.waitKey(0)
+        frame_id += 1
         if ch == 27 or ch == ord("q") or ch == ord("Q"):
             break
+    #write_results(result_filename, results)
 
 
 def imageflow_demo(predictor, vis_folder, current_time, args):
@@ -209,18 +261,40 @@ def imageflow_demo(predictor, vis_folder, current_time, args):
     vid_writer = cv2.VideoWriter(
         save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
     )
+    tracker = DETTracker(args, frame_rate=30)
+    timer = Timer()
+    frame_id = 0
+    results = []
     while True:
+        if frame_id % 20 == 0:
+            logger.info('Processing frame {} ({:.2f} fps)'.format(frame_id, 1. / max(1e-5, timer.average_time)))
         ret_val, frame = cap.read()
         if ret_val:
-            outputs, img_info = predictor.inference(frame)
-            result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
+            outputs, img_info = predictor.inference(frame, timer)
+            online_targets = tracker.update(outputs[0], [img_info['height'], img_info['width']], exp.test_size)
+            online_tlwhs = []
+            online_ids = []
+            online_scores = []
+            for t in online_targets:
+                tlwh = t.tlwh
+                tid = t.track_id
+                vertical = tlwh[2] / tlwh[3] > 1.6
+                if tlwh[2] * tlwh[3] > args.min_box_area and not vertical:
+                    online_tlwhs.append(tlwh)
+                    online_ids.append(tid)
+                    online_scores.append(t.score)
+            timer.toc()
+            results.append((frame_id + 1, online_tlwhs, online_ids, online_scores))
+            online_im = plot_tracking(img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id + 1,
+                                      fps=1. / timer.average_time)
             if args.save_result:
-                vid_writer.write(result_frame)
+                vid_writer.write(online_im)
             ch = cv2.waitKey(1)
             if ch == 27 or ch == ord("q") or ch == ord("Q"):
                 break
         else:
             break
+        frame_id += 1
 
 
 def main(exp, args):
@@ -232,7 +306,7 @@ def main(exp, args):
     os.makedirs(file_name, exist_ok=True)
 
     if args.save_result:
-        vis_folder = os.path.join(file_name, "vis_res")
+        vis_folder = os.path.join(file_name, "track_vis")
         os.makedirs(vis_folder, exist_ok=True)
 
     if args.trt:
@@ -268,6 +342,9 @@ def main(exp, args):
     if args.fuse:
         logger.info("\tFusing model...")
         model = fuse_model(model)
+    
+    if args.fp16:
+            model = model.half()  # to FP16
 
     if args.trt:
         assert not args.fuse, "TensorRT model is not support model fusing!"
@@ -282,7 +359,7 @@ def main(exp, args):
         trt_file = None
         decoder = None
 
-    predictor = Predictor(model, exp, COCO_CLASSES, trt_file, decoder, args.device)
+    predictor = Predictor(model, exp, COCO_CLASSES, trt_file, decoder, args.device, args.fp16)
     current_time = time.localtime()
     if args.demo == "image":
         image_demo(predictor, vis_folder, args.path, current_time, args.save_result)
