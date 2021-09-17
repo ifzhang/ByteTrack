cb05e00e55a738c087c186233281fc05b1f07131
Yifu Zhang <953260160@qq.com>
2021-09-17 17:42:21 +0800

    Update __init__.py

--- yolox/data/datasets/__init__.py
+++ yolox/data/datasets/__init__.py
@@ -2,9 +2,6 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
 
-from .coco import COCODataset
-from .coco_classes import COCO_CLASSES
 from .datasets_wrapper import ConcatDataset, Dataset, MixConcatDataset
 from .mosaicdetection import MosaicDetection
-from .voc import VOCDetection
 from .mot import MOTDataset
