472bb6707db0651f39010d3999c57656f5480ca6
zhangyifu.xyz <zhangyifu.xyz@bytedance.com>
2021-10-09 22:19:31 +0800

    add ncnn c++ demo

--- demo/ncnn/cpp/src/bytetrack.cpp
+++ demo/ncnn/cpp/src/bytetrack.cpp
@@ -18,18 +18,35 @@
 
 #if defined(USE_NCNN_SIMPLEOCV)
 #include "simpleocv.h"
+#include <opencv2/opencv.hpp>
 #else
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 #endif
 #include <float.h>
 #include <stdio.h>
 #include <vector>
+#include <chrono>
+#include "BYTETracker.h"
 
-#define YOLOX_NMS_THRESH  0.45 // nms threshold
-#define YOLOX_CONF_THRESH 0.25 // threshold of bounding box prob
-#define YOLOX_TARGET_SIZE 640  // target image size after resize, might use 416 for small model
+#define YOLOX_NMS_THRESH  0.7 // nms threshold
+#define YOLOX_CONF_THRESH 0.1 // threshold of bounding box prob
+#define INPUT_W 1088  // target image size after resize, might use 416 for small model
+#define INPUT_H 608
+
+Mat static_resize(Mat& img) {
+    float r = min(INPUT_W / (img.cols*1.0), INPUT_H / (img.rows*1.0));
+    // r = std::min(r, 1.0f);
+    int unpad_w = r * img.cols;
+    int unpad_h = r * img.rows;
+    Mat re(unpad_h, unpad_w, CV_8UC3);
+    resize(img, re, re.size());
+    Mat out(INPUT_H, INPUT_W, CV_8UC3, Scalar(114, 114, 114));
+    re.copyTo(out(Rect(0, 0, re.cols, re.rows)));
+    return out;
+}
 
 // YOLOX use the same focus in yolov5
 class YoloV5Focus : public ncnn::Layer
@@ -79,13 +96,6 @@ public:
 };
 
 DEFINE_LAYER_CREATOR(YoloV5Focus)
-
-struct Object
-{
-    cv::Rect_<float> rect;
-    int label;
-    float prob;
-};
 
 struct GridAndStride
 {
@@ -179,15 +189,16 @@ static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vecto
     }
 }
 
-static void generate_grids_and_stride(const int target_size, std::vector<int>& strides, std::vector<GridAndStride>& grid_strides)
+static void generate_grids_and_stride(const int target_w, const int target_h, std::vector<int>& strides, std::vector<GridAndStride>& grid_strides)
 {
     for (int i = 0; i < (int)strides.size(); i++)
     {
         int stride = strides[i];
-        int num_grid = target_size / stride;
-        for (int g1 = 0; g1 < num_grid; g1++)
+        int num_grid_w = target_w / stride;
+        int num_grid_h = target_h / stride;
+        for (int g1 = 0; g1 < num_grid_h; g1++)
         {
-            for (int g0 = 0; g0 < num_grid; g0++)
+            for (int g0 = 0; g0 < num_grid_w; g0++)
             {
                 GridAndStride gs;
                 gs.grid0 = g0;
@@ -246,60 +257,11 @@ static void generate_yolox_proposals(std::vector<GridAndStride> grid_strides, co
     } // point anchor loop
 }
 
-static int detect_yolox(const cv::Mat& bgr, std::vector<Object>& objects)
+static int detect_yolox(ncnn::Mat& in_pad, std::vector<Object>& objects, ncnn::Extractor ex, float scale)
 {
-    ncnn::Net yolox;
-
-    yolox.opt.use_vulkan_compute = true;
-    // yolox.opt.use_bf16_storage = true;
-
-    // Focus in yolov5
-    yolox.register_custom_layer("YoloV5Focus", YoloV5Focus_layer_creator);
-
-    // original pretrained model from https://github.com/Megvii-BaseDetection/YOLOX
-    // ncnn model param: https://github.com/Megvii-BaseDetection/storage/releases/download/0.0.1/yolox_s_ncnn.tar.gz
-    yolox.load_param("yolox.param");
-    yolox.load_model("yolox.bin");
-
-    int img_w = bgr.cols;
-    int img_h = bgr.rows;
-
-    int w = img_w;
-    int h = img_h;
-    float scale = 1.f;
-    if (w > h)
-    {
-        scale = (float)YOLOX_TARGET_SIZE / w;
-        w = YOLOX_TARGET_SIZE;
-        h = h * scale;
-    }
-    else
-    {
-        scale = (float)YOLOX_TARGET_SIZE / h;
-        h = YOLOX_TARGET_SIZE;
-        w = w * scale;
-    }
-    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, img_w, img_h, w, h);
-
-    // pad to YOLOX_TARGET_SIZE rectangle
-    int wpad = YOLOX_TARGET_SIZE - w;
-    int hpad = YOLOX_TARGET_SIZE - h;
-    ncnn::Mat in_pad;
-    // different from yolov5, yolox only pad on bottom and right side,
-    // which means users don't need to extra padding info to decode boxes coordinate.
-    ncnn::copy_make_border(in, in_pad, 0, hpad, 0, wpad, ncnn::BORDER_CONSTANT, 114.f);
-
-    // python 0-1 input tensor with rgb_means = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)
-    // so for 0-255 input image, rgb_mean should multiply 255 and norm should div by std.
-    const float mean_vals[3] = {255.f * 0.485f, 255.f * 0.456, 255.f * 0.406f};
-    const float norm_vals[3] = {1 / (255.f * 0.229f), 1 / (255.f * 0.224f), 1 / (255.f * 0.225f)};
-
-    in_pad.substract_mean_normalize(mean_vals, norm_vals);
-
-    ncnn::Extractor ex = yolox.create_extractor();
 
     ex.input("images", in_pad);
-
+    
     std::vector<Object> proposals;
 
     {
@@ -309,10 +271,9 @@ static int detect_yolox(const cv::Mat& bgr, std::vector<Object>& objects)
         static const int stride_arr[] = {8, 16, 32}; // might have stride=64 in YOLOX
         std::vector<int> strides(stride_arr, stride_arr + sizeof(stride_arr) / sizeof(stride_arr[0]));
         std::vector<GridAndStride> grid_strides;
-        generate_grids_and_stride(YOLOX_TARGET_SIZE, strides, grid_strides);
+        generate_grids_and_stride(INPUT_W, INPUT_H, strides, grid_strides);
         generate_yolox_proposals(grid_strides, out, YOLOX_CONF_THRESH, proposals);
     }
-
     // sort all proposals by score from highest to lowest
     qsort_descent_inplace(proposals);
 
@@ -334,10 +295,10 @@ static int detect_yolox(const cv::Mat& bgr, std::vector<Object>& objects)
         float y1 = (objects[i].rect.y + objects[i].rect.height) / scale;
 
         // clip
-        x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
-        y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
-        x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
-        y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
+        // x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
+        // y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
+        // x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
+        // y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
 
         objects[i].rect.x = x0;
         objects[i].rect.y = y0;
@@ -348,76 +309,106 @@ static int detect_yolox(const cv::Mat& bgr, std::vector<Object>& objects)
     return 0;
 }
 
-static void draw_objects(const cv::Mat& bgr, const std::vector<Object>& objects)
-{
-    static const char* class_names[] = {
-        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
-        "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
-        "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
-        "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-        "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
-        "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
-        "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
-        "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
-        "hair drier", "toothbrush"
-    };
-
-    cv::Mat image = bgr.clone();
-
-    for (size_t i = 0; i < objects.size(); i++)
-    {
-        const Object& obj = objects[i];
-
-        fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f x %.2f\n", obj.label, obj.prob,
-                obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height);
-
-        cv::rectangle(image, obj.rect, cv::Scalar(255, 0, 0));
-
-        char text[256];
-        sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
-
-        int baseLine = 0;
-        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-
-        int x = obj.rect.x;
-        int y = obj.rect.y - label_size.height - baseLine;
-        if (y < 0)
-            y = 0;
-        if (x + label_size.width > image.cols)
-            x = image.cols - label_size.width;
-
-        cv::rectangle(image, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-                      cv::Scalar(255, 255, 255), -1);
-
-        cv::putText(image, text, cv::Point(x, y + label_size.height),
-                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-    }
-
-    cv::imshow("image", image);
-    cv::waitKey(0);
-}
-
 int main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [videopath]\n", argv[0]);
         return -1;
     }
 
-    const char* imagepath = argv[1];
+    ncnn::Net yolox;
 
-    cv::Mat m = cv::imread(imagepath, 1);
-    if (m.empty())
+    //yolox.opt.use_vulkan_compute = true;
+    //yolox.opt.use_bf16_storage = true;
+    yolox.opt.num_threads = 20;
+    //ncnn::set_cpu_powersave(0);
+
+    //ncnn::set_omp_dynamic(0);
+    //ncnn::set_omp_num_threads(20);
+
+    // Focus in yolov5
+    yolox.register_custom_layer("YoloV5Focus", YoloV5Focus_layer_creator);
+
+    // original pretrained model from https://github.com/Megvii-BaseDetection/YOLOX
+    // ncnn model param: https://github.com/Megvii-BaseDetection/storage/releases/download/0.0.1/yolox_s_ncnn.tar.gz
+    yolox.load_param("bytetrack_s_op.param");
+    yolox.load_model("bytetrack_s_op.bin");
+    
+    ncnn::Extractor ex = yolox.create_extractor();
+    //ex.set_num_threads(20);
+
+    const char* videopath = argv[1];
+
+    VideoCapture cap(videopath);
+	if (!cap.isOpened())
+		return 0;
+
+	int img_w = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	int img_h = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+    int fps = cap.get(CV_CAP_PROP_FPS);
+    long nFrame = static_cast<long>(cap.get(CV_CAP_PROP_FRAME_COUNT));
+    cout << "Total frames: " << nFrame << endl;
+
+    VideoWriter writer("demo.mp4", CV_FOURCC('m', 'p', '4', 'v'), fps, Size(img_w, img_h));
+
+    Mat img;
+    BYTETracker tracker(fps, 30);
+    int num_frames = 0;
+    int total_ms = 1;
+	for (;;)
     {
-        fprintf(stderr, "cv::imread %s failed\n", imagepath);
-        return -1;
+        if(!cap.read(img))
+            break;
+        num_frames ++;
+        if (num_frames % 20 == 0)
+        {
+            cout << "Processing frame " << num_frames << " (" << num_frames * 1000000 / total_ms << " fps)" << endl;
+        }
+		if (img.empty())
+			break;
+
+        float scale = min(INPUT_W / (img.cols*1.0), INPUT_H / (img.rows*1.0));
+        Mat pr_img = static_resize(img);
+        ncnn::Mat in_pad = ncnn::Mat::from_pixels_resize(pr_img.data, ncnn::Mat::PIXEL_BGR2RGB, INPUT_W, INPUT_H, INPUT_W, INPUT_H);
+    
+        // python 0-1 input tensor with rgb_means = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)
+        // so for 0-255 input image, rgb_mean should multiply 255 and norm should div by std.
+        const float mean_vals[3] = {255.f * 0.485f, 255.f * 0.456, 255.f * 0.406f};
+        const float norm_vals[3] = {1 / (255.f * 0.229f), 1 / (255.f * 0.224f), 1 / (255.f * 0.225f)};
+
+        in_pad.substract_mean_normalize(mean_vals, norm_vals);
+
+        std::vector<Object> objects;
+        auto start = chrono::system_clock::now();
+        //detect_yolox(img, objects);
+        detect_yolox(in_pad, objects, ex, scale);
+        vector<STrack> output_stracks = tracker.update(objects);
+        auto end = chrono::system_clock::now();
+        total_ms = total_ms + chrono::duration_cast<chrono::microseconds>(end - start).count();
+        for (int i = 0; i < output_stracks.size(); i++)
+		{
+			vector<float> tlwh = output_stracks[i].tlwh;
+			bool vertical = tlwh[2] / tlwh[3] > 1.6;
+			if (tlwh[2] * tlwh[3] > 20 && !vertical)
+			{
+				Scalar s = tracker.get_color(output_stracks[i].track_id);
+				putText(img, format("%d", output_stracks[i].track_id), Point(tlwh[0], tlwh[1] - 5), 
+                        0, 0.6, Scalar(0, 0, 255), 2, LINE_AA);
+                rectangle(img, Rect(tlwh[0], tlwh[1], tlwh[2], tlwh[3]), s, 2);
+			}
+		}
+        putText(img, format("frame: %d fps: %d num: %d", num_frames, num_frames * 1000000 / total_ms, output_stracks.size()), 
+                Point(0, 30), 0, 0.6, Scalar(0, 0, 255), 2, LINE_AA);
+        writer.write(img);
+        char c = waitKey(1);
+        if (c > 0)
+        {
+            break;
+        }
     }
-
-    std::vector<Object> objects;
-    detect_yolox(m, objects);
-
-    draw_objects(m, objects);
+    cap.release();
+    cout << "FPS: " << num_frames * 1000000 / total_ms << endl;
 
     return 0;
 }
