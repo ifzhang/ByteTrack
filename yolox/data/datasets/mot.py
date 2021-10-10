1956faba9629114d7e0abf0f170cbf8f4635c589
zhangyifu.xyz <zhangyifu.xyz@bytedance.com>
2021-10-11 01:24:20 +0800

    update yolox

--- yolox/data/datasets/mot.py
+++ yolox/data/datasets/mot.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Copyright (c) Megvii, Inc. and its affiliates.
-
 import cv2
 import numpy as np
 from pycocotools.coco import COCO
--- yolox/evaluators/mot_evaluator.py
+++ yolox/evaluators/mot_evaluator.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Copyright (c) Megvii, Inc. and its affiliates.
-
 from collections import defaultdict
 from loguru import logger
 from tqdm import tqdm
