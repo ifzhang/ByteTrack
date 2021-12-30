8eea13a164bb8a44ca85b3d4177bef56a7841021
nagar502 <95431548+nagar502@users.noreply.github.com>
2021-12-30 21:44:43 +0530

    Update demo_track.py

--- tools/demo_track.py
+++ tools/demo_track.py
@@ -21,7 +21,7 @@ IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 def make_parser():
     parser = argparse.ArgumentParser("ByteTrack Demo!")
     parser.add_argument(
-        "demo", default="image", help="demo type, eg. image, video and webcam"
+        "--demo", default="image", help="demo type, eg. image, video and webcam"
     )
     parser.add_argument("-expn", "--experiment-name", type=str, default=None)
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
