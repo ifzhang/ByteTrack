e0ea907d83628e420c56f3ea16439357cb0d9e0d
sr99622 <54281528+sr99622@users.noreply.github.com>
2022-09-18 09:56:58 -0400

    Update byte_tracker.py
    pytorch uint8 to bool for deprecation

--- yolox/tracker/byte_tracker.py
+++ yolox/tracker/byte_tracker.py
@@ -178,7 +178,7 @@ class BYTETracker(object):
         inds_low = scores > 0.1
         inds_high = scores < self.args.track_thresh
 
-        inds_second = np.logical_and(inds_low, inds_high)
+        inds_second = np.logical_and(inds_low, inds_high).bool()
         dets_second = bboxes[inds_second]
         dets = bboxes[remain_inds]
         scores_keep = scores[remain_inds]
